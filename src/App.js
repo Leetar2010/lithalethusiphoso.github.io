51525d99f44dfa94693c5be779ef4c2a807a0b26
Leetar2010 <97743246+Leetar2010@users.noreply.github.com>
2025-11-05 17:42:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+      <h1>Hello, Lithalethu 👋🏽</h1>
+      <p>This is your personal portfolio website!</p>
+    </div>
+  );
+}
